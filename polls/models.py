8decf475826028bcd0017089886739e17ac27828
Alejandro Herrera Jiménez <alejandroherrera140697@gmail.com>
2025-03-05 12:36:28 +0100

    He modificado el models.py

--- polls/models.py
+++ polls/models.py
@@ -13,7 +13,6 @@ class Question(models.Model):
 
     def was_published_recently(self):
         now = timezone.now()
-a
 	return self.pub_date >= now - datetime.timedelta(days=1)
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
