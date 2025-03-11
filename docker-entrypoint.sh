df5b1948e0ab5987e37f454040389d5dbf769b7e
Alejandro Herrera Jiménez <alejandroherrera140697@gmail.com>
2025-03-11 08:49:20 +0100

    nuevos cambios

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,5 @@ cd /usr/src/app/
 export DJANGO_SUPERUSER_PASSWORD=$DJANGO_PASS
 sleep 20
 python3 manage.py migrate
-python manage.py createsuperuser --username=$DJANGO_USER --email=$DJANGO_EMAIL --noinput
+python manage.py createsuperuser --username=$DJANGO_USER --email=$DJANGO_MAIL --noinput
 python3 manage.py runserver 0.0.0.0:3000
